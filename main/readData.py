dda709dc4e65bdd24f26c61ffb2ea918d1ea90d0
dk67604 <dk67604@uga.edu>
2016-09-08 22:01:12 -0400

    Preprocessing of Small Dataset
    Following changes has been added:
    1) Added code for extraction of filename from Path to make as key for one of RDD
    2) Merged the filtered content of byte file with hash-file and label.

--- main/readData.py
+++ main/readData.py
@@ -2,64 +2,72 @@ import urllib
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext, Row
 import re
+import os
 
+sc = SparkContext(conf=SparkConf().setAppName("MalwareClassifier"))
 
-sc = SparkContext(conf = SparkConf().setAppName("Read Data"))
 
 def cleanDoc(bytefileData):
-	
-	# Removing unwanted items from the list.
-	filteredFile=re.sub("\?|\n|\r","",bytefileData)
+    # Removing unwanted items from the list.
+    filteredFile = re.sub("\?|\n|\r", "", bytefileData)
 
-	# Removing line pointers.
-	removePointer= [word.encode('utf-8') for word in filteredFile.split() if len(word)<3]
-	return removePointer
+    # Removing line pointers.
+    removePointer = [word.encode('utf-8') for word in filteredFile.split() if len(word) < 3]
+    return removePointer
 
 
 def main():
+    # =========================================================================
+    # Access Key and secret key necessary to read data from Amazon S3
+    # =========================================================================
+    sc._jsc.hadoopConfiguration().set('fs.s3n.awsAccessKeyId', 'AKIAI6K6FXFJLKCKQBWA')
+    sc._jsc.hadoopConfiguration().set('fs.s3n.awsSecretAccessKey', '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')
 
-	#=========================================================================
-	# Access Key and secret key necessary to read data from Amazon S3
-	#=========================================================================
-	sc._jsc.hadoopConfiguration().set('fs.s3n.awsAccessKeyId','AKIAI6K6FXFJLKCKQBWA')
-	sc._jsc.hadoopConfiguration().set('fs.s3n.awsSecretAccessKey','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')
-	
-	#=========================================================================
-	# Reading training file from s3
-	#=========================================================================
-	hashFileData= sc.textFile("s3n://eds-uga-csci8360/data/project2/labels/X_train_small.txt").map(lambda doc:doc.encode("utf-8").strip()).cache()
-	#entirehashFileData = hashFileData.zipWithIndex().map(lambda doc:(doc[1],doc[0])).cache()
+    # =========================================================================
+    # Reading training file from s3
+    # =========================================================================
+    hashFileData = sc.textFile("s3n://eds-uga-csci8360/data/project2/labels/X_train_small.txt").map(lambda doc: doc.encode("utf-8").strip())
+    entirehashFileData = hashFileData.zipWithIndex().map(lambda doc:(doc[1],doc[0])).cache()
 
-	#=========================================================================
-	# Reading (hashcode).bytes file from s3:
-	# 1. Concatenating .bytes extension to each hashcode obtained.
-	# 2. Making RDD a string to pass tp wholeTextFile function.
-	# 3. Read bytes file from s3 and stored it in RDD format (Filename, FileData)
-	# 4. Doing initial cleaning of the data through function cleanDoc()
-	#=========================================================================
-	byteFile= hashFileData.map(lambda doc: ("s3n://eds-uga-csci8360/data/project2/binaries/"+doc+".bytes"))
-	filePath=byteFile.reduce(lambda str1,str2 : str1 + "," + str2)
-   	byteFileCollect= sc.wholeTextFiles(filePath,50)
-   	#======
-   	# Use the below line to test data of byte file
-   	#byteFileCollect= sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes",50)
-   	#======
-	cleanFile= byteFileCollect.map(lambda doc:(doc[0].encode('utf-8'),cleanDoc(doc[1]))).cache()
-	#cleanFile.saveAsTextFile("C:\\Users\Shubhi\Desktop\cleanFile.txt")
-	
-	#=========================================================================
-	# Reading label file from s3
-	#=========================================================================
-	labelData= sc.textFile("s3n://eds-uga-csci8360/data/project2/labels/y_train_small.txt").map(lambda doc:doc.encode("utf-8").strip()).cache()
-	entireLabelData = labelData.zipWithIndex().map(lambda doc:(doc[1],doc[0])).cache()
-	
-	#=========================================================================
-	# Joining RDD's
-	#=========================================================================
-	# fileAndLabelRDD= entirehashFileData.join(entireLabelData)
-	# labelsAndInputJoinedRDD = fileAndLabelRDD.map(lambda (x,y):(x,y[0],y[1]))
+    # =========================================================================
+    # Reading (hashcode).bytes file from s3:
+    # 1. Concatenating .bytes extension to each hashcode obtained.
+    # 2. Making RDD a string to pass tp wholeTextFile function.
+    # 3. Read bytes file from s3 and stored it in RDD format (Filename, FileData)
+    # 4. Doing initial cleaning of the data through function cleanDoc()
+    # =========================================================================
+    byteFile = hashFileData.map(lambda doc: ("s3n://eds-uga-csci8360/data/project2/binaries/" + doc + ".bytes"))
+    filePath = byteFile.reduce(lambda str1, str2: str1 + "," + str2)
+    byteFileCollect = sc.wholeTextFiles(filePath, 50)
+    #byteFileCollect = sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/hkscPWGaIw0ALpHuNKr8.bytes")
+    # ======
+    # Use the below line to test data of byte file
+    # byteFileCollect= sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes",50)
+    # ======
+    cleanFile = byteFileCollect.map(lambda doc: (doc[0].encode('utf-8'), cleanDoc(doc[1]))).cache()
+    wholeTextFileNameRDD = cleanFile.map(lambda (x, y): (os.path.splitext(os.path.basename(x))[0], y))
 
-	
-	
+
+    # cleanFile.saveAsTextFile("C:\\Users\Shubhi\Desktop\cleanFile.txt")
+
+    # =========================================================================
+    # Reading label file from s3
+    # =========================================================================
+    labelData = sc.textFile("s3n://eds-uga-csci8360/data/project2/labels/y_train_small.txt").map(lambda doc: doc.encode("utf-8").strip()).cache()
+    entireLabelData = labelData.zipWithIndex().map(lambda doc: (doc[1], doc[0])).cache()
+
+
+# =========================================================================
+# Joining RDD's of HashFile,Label and content
+# =========================================================================
+# fileAndLabelRDD= entirehashFileData.join(entireLabelData)
+# labelsAndInputJoinedRDD = fileAndLabelRDD.map(lambda (x,y):(x,y[0],y[1]))
+    hashFileLablePair=entirehashFileData.join(entireLabelData)
+    hashFileLableRDD=hashFileLablePair.values()
+    hashFileLableRDDPair=hashFileLableRDD.keyBy(lambda line:line[0]).mapValues(lambda line:line[1])
+    finalDataSetRDD = hashFileLableRDDPair.join(wholeTextFileNameRDD)
+    print(finalDataSetRDD.values().take(1))
+
+
 if __name__ == "__main__":
     main()
