c5936e64bfbd77befd45a5528227fd1a783207e1
shubhi2108 <shubhi.j08@gmail.com>
2016-09-09 23:14:13 -0400

    Created Datafram and generated ngrams

--- main/readData.py
+++ main/readData.py
@@ -1,10 +1,17 @@
+from __future__ import print_function
 import urllib
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext, Row,SparkSession
+from pyspark.sql.types import *
+from pyspark.ml.feature import NGram
 import re
 import os
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import StringIndexer, VectorIndexer
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
-warehouseLocation = 'file:////home/dharamendra/PycharmProjects/MalwareDetection'
+warehouseLocation = 'C:\DSP_Project2\days-project2'
 #sc = SparkContext(conf=SparkConf().setAppName("MalwareClassifier"))
 spark = SparkSession\
         .builder\
@@ -46,16 +53,15 @@ def main():
     byteFile = hashFileData.map(lambda doc: ("s3n://eds-uga-csci8360/data/project2/binaries/" + doc + ".bytes"))
     filePath = byteFile.reduce(lambda str1, str2: str1 + "," + str2)
     byteFileCollect = sc.wholeTextFiles(filePath, 50)
-    #byteFileCollect = sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/hkscPWGaIw0ALpHuNKr8.bytes")
+    #byteFileCollect = sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes")
+
     # ======
     # Use the below line to test data of byte file
     # byteFileCollect= sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes",50)
     # ======
     cleanFile = byteFileCollect.map(lambda doc: (doc[0].encode('utf-8'), cleanDoc(doc[1]))).cache()
     wholeTextFileNameRDD = cleanFile.map(lambda (x, y): (os.path.splitext(os.path.basename(x))[0], y))
-
-
-    # cleanFile.saveAsTextFile("C:\\Users\Shubhi\Desktop\cleanFile.txt")
+    #cleanFile.saveAsTextFile("C:\\Users\Shubhi\Desktop\cleanFile.txt")
 
     # =========================================================================
     # Reading label file from s3
@@ -64,17 +70,65 @@ def main():
     entireLabelData = labelData.zipWithIndex().map(lambda doc: (doc[1], doc[0])).cache()
 
 
-# =========================================================================
-# Joining RDD's of HashFile,Label and content
-# =========================================================================
-# fileAndLabelRDD= entirehashFileData.join(entireLabelData)
-# labelsAndInputJoinedRDD = fileAndLabelRDD.map(lambda (x,y):(x,y[0],y[1]))
+    # =========================================================================
+    # Joining RDD's of HashFile,Label and content
+    # =========================================================================
+    # fileAndLabelRDD= entirehashFileData.join(entireLabelData)
+    # labelsAndInputJoinedRDD = fileAndLabelRDD.map(lambda (x,y):(x,y[0],y[1]))
     hashFileLablePair=entirehashFileData.join(entireLabelData)
     hashFileLableRDD=hashFileLablePair.values()
     hashFileLableRDDPair=hashFileLableRDD.keyBy(lambda line:line[0]).mapValues(lambda line:line[1])
-    finalDataSetRDD = hashFileLableRDDPair.join(wholeTextFileNameRDD)
-    print(finalDataSetRDD.values().take(1))
+    dataSet = hashFileLableRDDPair.join(wholeTextFileNameRDD)
+    finalDataSetRDD= dataSet.map(lambda (x,y): (x,y[0],y[1]))
+    
+    # =========================================================================
+    # 1. creating DATAFRAME
+    # 2. Generating NGRAMS
+    # =========================================================================
+    schemaString = "HashCodeFile Label Content"
+    fields = [StructField("HashCodeFile", StringType(), True),StructField("Label", StringType(), True),StructField("Content",ArrayType(StringType(),False), True) ]
+    schema = StructType(fields)
+    schemaByte = spark.createDataFrame(finalDataSetRDD,schema)
+    schemaByte.createOrReplaceTempView("byteDataFrame")
+    ngram = NGram(n=4, inputCol="Content", outputCol="ngrams")
+    ngramDataFrame = ngram.transform(schemaByte)
+    #ngramDataFrame.select("ngrams").show(truncate=False)   \\No Use
+    #ngramDataFrame.show()
 
+    # =========================================================================
+    # RANDOM FOREST
+    # =========================================================================
+    
+    # labelIndexer = StringIndexer(inputCol="Label", outputCol="indexedLabel").fit(schemaByte)
+    # # Automatically identify categorical features, and index them.
+    # # Set maxCategories so features with > 4 distinct values are treated as continuous.
+    # featureIndexer = VectorIndexer(inputCol="Content", outputCol="indexedFeatures", maxCategories=9).fit(schemaByte)
+
+    # (trainingData, testData) = schemaByte.randomSplit([0.7, 0.3])
+
+    # # Train a RandomForest model.
+    # rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
+
+    # # Chain indexers and forest in a Pipeline
+    # pipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf])
+
+    # # Train model.  This also runs the indexers.
+    # model = pipeline.fit(trainingData)
+
+    # # Make predictions.
+    # predictions = model.transform(testData)
+
+    # # Select example rows to display.
+    # predictions.select("prediction", "indexedLabel", "features").show(5)
+
+    # # Select (prediction, true label) and compute test error
+    # evaluator = MulticlassClassificationEvaluator(
+    #     labelCol="indexedLabel", predictionCol="prediction", metricName="precision")
+    # accuracy = evaluator.evaluate(predictions)
+    # print("Test Error = %g" % (1.0 - accuracy))
+
+    # rfModel = model.stages[2]
+    # print(rfModel)  # summary only
 
 if __name__ == "__main__":
     main()
