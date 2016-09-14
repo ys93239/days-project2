4028431840da6bbe98930f1befb80927c98e2767
dk67604 <dk67604@uga.edu>
2016-09-14 12:06:13 -0400

    Updated code for Random Forest using ML library.
    Added the concept of Spark pipelining, TF-IDF, Random Forest

--- main/readData.py
+++ main/readData.py
@@ -2,13 +2,19 @@ from __future__ import print_function
 import urllib
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext, Row,SparkSession
-from pyspark.ml.feature import IDF, Tokenizer
-from pyspark.mllib.feature import HashingTF,IDF
+from pyspark.ml.feature import IDF, Tokenizer,HashingTF
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+#from pyspark.mllib.feature import HashingTF,IDF
 from pyspark.sql.types import *
 from pyspark.ml.feature import NGram
-from pyspark.mllib.feature import Word2Vec
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.tree import RandomForest, RandomForestModel
+from pyspark import StorageLevel
+#from pyspark.mllib.feature import Word2Vec
+#from pyspark.mllib.regression import LabeledPoint
+#from pyspark.mllib.tree import RandomForest, RandomForestModel
+#from pyspark.mllib.feature import PCA as PCAmllib
 import re
 import os
 
@@ -30,6 +36,14 @@ def cleanDoc(bytefileData):
     # Removing line pointers.
     removePointer = [word.encode('utf-8') for word in filteredFile.split() if len(word) < 3]
     return removePointer
+
+def uniqueByte(inputByte):
+    x= list(set(inputByte))
+    temp=[]
+    for str in x:
+        tempStr=str.replace(" ","")
+        temp.append(tempStr)
+    return temp
 
 
 def main():
@@ -54,13 +68,15 @@ def main():
     # =========================================================================
     byteFile = hashFileData.map(lambda doc: ("s3n://eds-uga-csci8360/data/project2/binaries/" + doc + ".bytes"))
     filePath = byteFile.reduce(lambda str1, str2: str1 + "," + str2)
-    #byteFileCollect = sc.wholeTextFiles(filePath, 20000)
-    byteFileCollect = sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/hkscPWGaIw0ALpHuNKr8.bytes,s3n://eds-uga-csci8360/data/project2/binaries/G31czXvpnwUfRtdJ4TFs.bytes,s3n://eds-uga-csci8360/data/project2/binaries/dETSCuIZDapLP9AlJ7o6.bytes,s3n://eds-uga-csci8360/data/project2/binaries/F3Zj217CLRxgi0NyHMY4.bytes,s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes")
+    byteFileCollect = sc.wholeTextFiles(filePath, 100)
+    parquetFileDF = spark.read.parquet("C:\\Users\Shubhi\Desktop\cleanFile.parquet")
+    #byteFileCollect = sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/hkscPWGaIw0ALpHuNKr8.bytes,s3n://eds-uga-csci8360/data/project2/binaries/G31czXvpnwUfRtdJ4TFs.bytes,s3n://eds-uga-csci8360/data/project2/binaries/dETSCuIZDapLP9AlJ7o6.bytes,s3n://eds-uga-csci8360/data/project2/binaries/F3Zj217CLRxgi0NyHMY4.bytes,s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes")
+
     # ======
     # Use the below line to test data of byte file
     # byteFileCollect= sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes",50)
     # ======
-    cleanFile = byteFileCollect.map(lambda doc: (doc[0].encode('utf-8'), cleanDoc(doc[1]))).cache()
+    cleanFile = byteFileCollect.map(lambda doc: (doc[0].encode('utf-8'), cleanDoc(doc[1])))
     wholeTextFileNameRDD = cleanFile.map(lambda (x, y): (os.path.splitext(os.path.basename(x))[0], y))
 
 
@@ -89,52 +105,86 @@ def main():
     # =========================================================================
     schemaString = "hashcodefile label content"
     fields = [StructField("hashcodefile", StringType(), True), StructField("label", StringType(), True),
-              StructField("content", ArrayType(StringType(), False), True)]
+              StructField("content", ArrayType(StringType(), False),True)]
     schema = StructType(fields)
+    fieldsDF = [StructField("hashcodefile", StringType(), True), StructField("label", StringType(), True),
+                StructField("2-grams", StringType(), False)]
+    schemaDF = StructType(fieldsDF)
     schemaByte = spark.createDataFrame(finalDataSetRDD, schema)
+    #schemaByte.write.parquet("file:///home/dharamendra/parque-dataset.parquet")
+    #schemaByteParque=spark.read.parquet("file:///home/dharamendra/parque-dataset.parquet")
     schemaByte.createOrReplaceTempView("byteDataFrame")
     ngram = NGram(n=2, inputCol="content", outputCol="2-grams")
     ngramDataFrame = ngram.transform(schemaByte).select("hashcodefile","label","2-grams")
-    ngramDataFrameString=ngramDataFrame.withColumn("2-gramString",ngramDataFrame["2-grams"].cast(StringType())).select("label","2-gramString")
+    ngramRDD=ngramDataFrame.rdd
+    ngramRDDUnique = ngramRDD.map(lambda line: (line[0].encode('utf-8'),line[1], uniqueByte(line[2])))
+    ngramDFUnique=spark.createDataFrame(ngramRDDUnique,schemaDF)
+    #ngramDFUnique.show()
+    #ngramDataFrameRDDUnique.saveAsTextFile("file:///home/dharamendra/unique.txt");
+    ngramDataFrameStringRDD=ngramRDDUnique.map(lambda line:(line[1].encode('utf-8'),' '.join(str(x) for x in line[2])))
+    fieldTF = [StructField("label", StringType(), True), StructField("2-grams", StringType(), True)]
+    schemaTF = StructType(fieldTF)
+    inputTFDF=spark.createDataFrame(ngramDataFrameStringRDD,schemaTF)
+    #ngramDataFrameStringRDD.saveAsTextFile("file:///home/dharamendra/unique.txt");
 
-    ngramDataFrameStringRDD=ngramDataFrameString.rdd
-    temp=ngramDataFrameStringRDD.flatMap(lambda line:line[1].split(",")).map(lambda x: (int(str(x.replace(" ", "")).encode('hex'),16), 1)) .reduceByKey(lambda x,y:x+y).map(lambda x:(x[1],x[0]))
-    #temp.saveAsTextFile("file:///home/dharamendra/t7.txt")
-    # tempDF=temp.toDF("frequency","2-gram")
-    fields = [StructField("frequency", LongType(), True), StructField("2-gram", LongType(), True)]
-    schemaFreq = StructType(fields)
-    tempDF = spark.createDataFrame(temp, schemaFreq)
-    tempDF.createOrReplaceTempView("frequencyView")
-    count=spark.sql("SELECT SUM(frequency) AS frequencySum from frequencyView")
-    #count.rdd.saveAsTextFile("file:///home/dharamendra/t6.txt")
-    #==============================================================================
-    #
-    #Term Frequequency
-    #=====================================================
-    #
-    hashingTF = HashingTF(35468)
-    data_hashed = ngramDataFrameStringRDD.map(lambda (label, text): LabeledPoint(label, hashingTF.transform(text)))
-    data_hashed.persist()
-    #idf = IDF().fit(data_hashed)
-    #idfIgnore = IDF(minDocFreq=2).fit(data_hashed)
-    #tfidfIgnore = idf.transform(data_hashed)
-    #tfidfIgnore.saveAsTextFile("file:///home/dharamendra/t9.txt")
+    count=ngramRDDUnique.flatMap(lambda line:(' '.join(str(x) for x in line[2])).split(" ")).distinct().count()
+    print ("Count:",count)
+    #count.saveAsTextFile("file:///home/dharamendra/unique.txt");
+
+    # #==============================================================================
+    # #
+    # #Term Frequequency
+    # #=====================================================
+    # #
+    tokenizer = Tokenizer(inputCol="2-grams", outputCol="bytes")
+    inputData = tokenizer.transform(inputTFDF)
+    hashingTF = HashingTF(inputCol="bytes", outputCol="bytesFeatures", numFeatures=count)
+    featurizedData = hashingTF.transform(inputData)
+    # alternatively, CountVectorizer can also be used to get term frequency vectors
+
+    idf = IDF(inputCol="bytesFeatures", outputCol="features")
+    idfModel = idf.fit(featurizedData)
+    rescaledData = idfModel.transform(featurizedData)
+    # for features_label in rescaledData.select("features", "label").take(3):
+    #     print(features_label)
+
+    labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(rescaledData)
+
+    # Automatically identify categorical features, and index them.
+    # Set maxCategories so features with > 4 distinct values are treated as continuous.
+    featureIndexer = \
+        VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=5).fit(rescaledData)
+
     # Split the data into training and test sets (30% held out for testing)
-    (trainingData, testData) = data_hashed.randomSplit([0.7, 0.3])
-    #
-    model = RandomForest.trainClassifier(trainingData, numClasses=10, categoricalFeaturesInfo={10:2},
-                                          numTrees=5, featureSubsetStrategy="auto",
-                                          impurity='gini', maxDepth=7, maxBins=16)
-    #
-    # # Evaluate model on test instances and compute test error
-    predictions = model.predict(testData.map(lambda x: x.features))
-    labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
-    testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
-    print('Test Error = ' + str(testErr))
-    print('Learned classification forest model:')
-    print(model.toDebugString())
-    model.save(sc, "file:///home/dharamendra/myRandomForestClassificationModel")
-    sameModel = RandomForestModel.load(sc, "file:///home/dharamendra/myRandomForestClassificationModel")
+    (trainingData, testData) = rescaledData.randomSplit([0.7, 0.3])
+
+    # Train a RandomForest model.
+    rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", numTrees=5)
+
+    # Convert indexed labels back to original labels.
+    labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
+                                   labels=labelIndexer.labels)
+
+    # Chain indexers and forest in a Pipeline
+    pipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf, labelConverter])
+
+    # Train model.  This also runs the indexers.
+    model = pipeline.fit(trainingData)
+
+    # Make predictions.
+    predictions = model.transform(testData)
+
+    # Select example rows to display.
+    predictions.select("predictedLabel", "label", "features").show(5)
+
+    # Select (prediction, true label) and compute test error
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+    accuracy = evaluator.evaluate(predictions)
+    print("Test Error = %g" % (1.0 - accuracy))
+
+    rfModel = model.stages[2]
+    print(rfModel)
     #============================================================
 if __name__ == "__main__":
     main()
