9c9370f393880107220138f6e048408ccfbe89b3
dk67604 <dk67604@uga.edu>
2016-09-16 00:24:41 -0400

    Code change for creating parquet fille
    Code changes are done for creation  of parquet file after per-processing of training/testing dataset. The process accelerated the classification process used for Malware Classifier

--- main/readAndCleanData.py
+++ main/readAndCleanData.py
@@ -1,24 +1,15 @@
 from __future__ import print_function
-import urllib
-from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext, Row,SparkSession
-from pyspark.ml.feature import IDF, Tokenizer
-from pyspark.mllib.feature import HashingTF,IDF
 from pyspark.sql.types import *
-from pyspark.ml.feature import NGram
-from pyspark.mllib.feature import Word2Vec
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.tree import RandomForest, RandomForestModel
 import re
 import os
 
-#warehouseLocation = 'file:///home/dharamendra/PycharmProjects/MalwareDetection'
-warehouseLocation = 'C:\DSP_Project2\days-project2'
+
+#warehouseLocation = 'C:\DSP_Project2\days-project2'
 #sc = SparkContext(conf=SparkConf().setAppName("MalwareClassifier"))
 spark = SparkSession\
         .builder\
-        .appName("NGramExample")\
-        .config('spark.sql.warehouse.dir',warehouseLocation)\
+        .appName("MalwareClassification")\
         .getOrCreate()
 sc=spark.sparkContext;
 sqlContext=SQLContext(sc)
@@ -53,39 +44,40 @@ def main():
     # 3. Read bytes file from s3 and stored it in RDD format (Filename, FileData)
     # 4. Doing initial cleaning of the data through function cleanDoc()
     # =========================================================================
+    
     byteFile = hashFileData.map(lambda doc: ("s3n://eds-uga-csci8360/data/project2/binaries/" + doc + ".bytes"))
     filePath = byteFile.reduce(lambda str1, str2: str1 + "," + str2)
-    byteFileCollect = sc.wholeTextFiles(filePath, 20000)
+    
+    byteFileCollect = sc.wholeTextFiles(filePath, 1000)
+    #byteFileCollect.cache()
+    print ("Step 1 done")
+    #byteFileCollect.repartition(100)
     #byteFileCollect = sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/hkscPWGaIw0ALpHuNKr8.bytes,s3n://eds-uga-csci8360/data/project2/binaries/G31czXvpnwUfRtdJ4TFs.bytes,s3n://eds-uga-csci8360/data/project2/binaries/dETSCuIZDapLP9AlJ7o6.bytes,s3n://eds-uga-csci8360/data/project2/binaries/F3Zj217CLRxgi0NyHMY4.bytes,s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes")
     
     # ======
     # Use the below line to test data of byte file
     #byteFileCollect= sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes",50)
     # ======
-    cleanFile = byteFileCollect.map(lambda doc: (doc[0].encode('utf-8'), cleanDoc(doc[1])))
-    wholeTextFileNameRDD = cleanFile.map(lambda (x, y): (os.path.splitext(os.path.basename(x))[0], y))
-    # schemaString = "filename content"
-    # fields = [StructField("filename", StringType(), True),
-    #           StructField("content", ArrayType(StringType(), False), True)]
-    # schema = StructType(fields)
-    # schemaByte = spark.createDataFrame(wholeTextFileNameRDD, schema)
+    cleanFile = byteFileCollect.mapPartitions(lambda doc: (doc[0].encode('utf-8'), cleanDoc(doc[1])))
+    wholeTextFileNameRDD = cleanFile.mapPartitions(lambda (x, y): (os.path.splitext(os.path.basename(x))[0], y))
+    print("Step 2 done")
 
     # =========================================================================
     # Reading label file from s3
     # =========================================================================
     labelData = sc.textFile("s3n://eds-uga-csci8360/data/project2/labels/y_train_small.txt").map(lambda doc: doc.encode("utf-8").strip()).cache()
     entireLabelData = labelData.zipWithIndex().map(lambda doc: (doc[1], doc[0]))
-
+    print("Step 3 done")
 	# =========================================================================
 	# Joining RDD's of HashFile,Label and content
 	# =========================================================================
 	
-    hashFileLablePair=entirehashFileData.join(entireLabelData)
+    hashFileLablePair=entirehashFileData.join(entireLabelData,numPartitions=1000)
     hashFileLableRDD=hashFileLablePair.values()
     hashFileLableRDDPair=hashFileLableRDD.keyBy(lambda line:line[0]).mapValues(lambda line:line[1])
-    dataSet = hashFileLableRDDPair.join(wholeTextFileNameRDD)
+    dataSet = hashFileLableRDDPair.join(wholeTextFileNameRDD,numPartitions=100)
     finalDataSetRDD = dataSet.map(lambda (x, y): (x, y[0], y[1]))
-
+    print("Step 4 done")
     # =========================================================================
     # creating DATAFRAME
     # =========================================================================
@@ -98,4 +90,9 @@ def main():
     # =========================================================================
     # Reading and wrir=ting to Parquet file file from s3
     # =========================================================================
-    schemaByte.write.parquet("C:\\Users\Shubhi\Desktop\cleanFile.parquet")     #"Change the address pointng to S3"
+    print("Step 5 done")
+    schemaByte.write.parquet("cleanFile.parquet")
+    print("Step 6 done")
+             #Change the address pointng to S3"
+if __name__ == "__main__":
+    main()
