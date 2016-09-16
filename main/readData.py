2f15569e2578eb50452fb5f98d9b6c1754386a23
dk67604 <dk67604@uga.edu>
2016-09-16 00:19:29 -0400

    Added changes for Random Forest
    This version of code ran successfully for small training data set on EC2 machine.

--- main/readData.py
+++ main/readData.py
@@ -1,26 +1,28 @@
 from __future__ import print_function
 import urllib
-from pyspark import SparkContext, SparkConf
+
 from pyspark.sql import SQLContext, Row,SparkSession
-from pyspark.sql.types import *
-from pyspark.ml.feature import NGram
-import re
-import os
+from pyspark.ml.feature import IDF, Tokenizer,HashingTF
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
-from pyspark.ml.feature import StringIndexer, VectorIndexer
+from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.sql.types import *
+from pyspark.ml.feature import NGram
 
-warehouseLocation = 'C:\DSP_Project2\days-project2'
+import re
+import os
+
+warehouseLocation = 'file:///home/dharamendra/PycharmProjects/MalwareDetection'
 #sc = SparkContext(conf=SparkConf().setAppName("MalwareClassifier"))
 spark = SparkSession\
         .builder\
-        .appName("MalwareDetection")\
-        .config('spark.sql.warehouse.dir',warehouseLocation)\
+        .appName("MalwareClassification")\
         .getOrCreate()
 sc=spark.sparkContext;
+sqlContext=SQLContext(sc)
 
-
+#######Method for Preprocessing
 def cleanDoc(bytefileData):
     # Removing unwanted items from the list.
     filteredFile = re.sub("\?|\n|\r", "", bytefileData)
@@ -29,106 +31,97 @@ def cleanDoc(bytefileData):
     removePointer = [word.encode('utf-8') for word in filteredFile.split() if len(word) < 3]
     return removePointer
 
+def uniqueByte(inputByte):
+    x= list(set(inputByte))
+    temp=[]
+    for str in x:
+        tempStr=str.replace(" ","")
+        temp.append(tempStr)
+    return temp
 
+########Main method for Ma
 def main():
-    # =========================================================================
-    # Access Key and secret key necessary to read data from Amazon S3
-    # =========================================================================
-    sc._jsc.hadoopConfiguration().set('fs.s3n.awsAccessKeyId', 'AKIAI6K6FXFJLKCKQBWA')
-    sc._jsc.hadoopConfiguration().set('fs.s3n.awsSecretAccessKey', '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')
 
-    # =========================================================================
-    # Reading training file from s3
-    # =========================================================================
-    hashFileData = sc.textFile("s3n://eds-uga-csci8360/data/project2/labels/X_train_small.txt").map(lambda doc: doc.encode("utf-8").strip())
-    entirehashFileData = hashFileData.zipWithIndex().map(lambda doc:(doc[1],doc[0])).cache()
+    fieldsDF = [StructField("hashcodefile", StringType(), True), StructField("label", StringType(), True),
+                StructField("2-grams", StringType(), False)]
+    schemaDF = StructType(fieldsDF)
+    schemaByteParque=spark.read.parquet("cleanFile.parquet")
+    print ("Parquet File read completed")
+    schemaByteParque.createOrReplaceTempView("byteDataFrame")
+    ngram = NGram(n=2, inputCol="content", outputCol="2-grams")
+    ngramDataFrame = ngram.transform(schemaByteParque).select("hashcodefile","label","2-grams")
+    print ("N-gram completed")
+    ngramRDD=ngramDataFrame.rdd
+    ngramRDDUnique = ngramRDD.map(lambda line: (line[0].encode('utf-8'),line[1], uniqueByte(line[2])))
+    print ("N-Gram unique completed")
+    #ngramDFUnique=spark.createDataFrame(ngramRDDUnique,schemaDF)
+    #ngramDFUnique.show()
+    #ngramDataFrameRDDUnique.saveAsTextFile("file:///home/dharamendra/unique.txt");
+    ngramDataFrameStringRDD=ngramRDDUnique.map(lambda line:(line[1].encode('utf-8'),' '.join(str(x) for x in line[2])))
+    print ("Array type to String Type conversion completed")
+    fieldTF = [StructField("label", StringType(), True), StructField("2-grams", StringType(), True)]
+    schemaTF = StructType(fieldTF)
+    inputTFDF=spark.createDataFrame(ngramDataFrameStringRDD,schemaTF)
+    #ngramDataFrameStringRDD.saveAsTextFile("file:///home/dharamendra/unique.txt");
 
-    # =========================================================================
-    # Reading (hashcode).bytes file from s3:
-    # 1. Concatenating .bytes extension to each hashcode obtained.
-    # 2. Making RDD a string to pass tp wholeTextFile function.
-    # 3. Read bytes file from s3 and stored it in RDD format (Filename, FileData)
-    # 4. Doing initial cleaning of the data through function cleanDoc()
-    # =========================================================================
-    byteFile = hashFileData.map(lambda doc: ("s3n://eds-uga-csci8360/data/project2/binaries/" + doc + ".bytes"))
-    filePath = byteFile.reduce(lambda str1, str2: str1 + "," + str2)
-    byteFileCollect = sc.wholeTextFiles(filePath, 50)
-    #byteFileCollect = sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes")
+    count=ngramRDDUnique.flatMap(lambda line:(' '.join(str(x) for x in line[2])).split(" ")).distinct().count()
+    print ("Count:",count)
+    #count.saveAsTextFile("file:///home/dharamendra/unique.txt");
 
-    # ======
-    # Use the below line to test data of byte file
-    # byteFileCollect= sc.wholeTextFiles("s3n://eds-uga-csci8360/data/project2/binaries/c2hn9edSNJKmw0OukrBv.bytes",50)
-    # ======
-    cleanFile = byteFileCollect.map(lambda doc: (doc[0].encode('utf-8'), cleanDoc(doc[1]))).cache()
-    wholeTextFileNameRDD = cleanFile.map(lambda (x, y): (os.path.splitext(os.path.basename(x))[0], y))
-    #cleanFile.saveAsTextFile("C:\\Users\Shubhi\Desktop\cleanFile.txt")
+    # #==============================================================================
+    # #
+    # #Term Frequequency
+    # #=====================================================
+    # #
+    tokenizer = Tokenizer(inputCol="2-grams", outputCol="bytes")
+    inputData = tokenizer.transform(inputTFDF)
+    hashingTF = HashingTF(inputCol="bytes", outputCol="bytesFeatures", numFeatures=count)
+    featurizedData = hashingTF.transform(inputData)
+    print ("Term Frequency completed")
 
-    # =========================================================================
-    # Reading label file from s3
-    # =========================================================================
-    labelData = sc.textFile("s3n://eds-uga-csci8360/data/project2/labels/y_train_small.txt").map(lambda doc: doc.encode("utf-8").strip()).cache()
-    entireLabelData = labelData.zipWithIndex().map(lambda doc: (doc[1], doc[0])).cache()
+    idf = IDF(inputCol="bytesFeatures", outputCol="features")
+    idfModel = idf.fit(featurizedData)
+    rescaledData = idfModel.transform(featurizedData)
+    for features_label in rescaledData.select("features", "label").take(3):
+         print(features_label)
+    ######## Code for Random Forest Classifier
+    labelIndexer = StringIndexer(inputCol="label", outputCol="indexedLabel").fit(rescaledData)
 
+    # Automatically identify categorical features, and index them.
+    # Set maxCategories so features with > 4 distinct values are treated as continuous.
+    featureIndexer = \
+        VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=5).fit(rescaledData)
 
-    # =========================================================================
-    # Joining RDD's of HashFile,Label and content
-    # =========================================================================
-    # fileAndLabelRDD= entirehashFileData.join(entireLabelData)
-    # labelsAndInputJoinedRDD = fileAndLabelRDD.map(lambda (x,y):(x,y[0],y[1]))
-    hashFileLablePair=entirehashFileData.join(entireLabelData)
-    hashFileLableRDD=hashFileLablePair.values()
-    hashFileLableRDDPair=hashFileLableRDD.keyBy(lambda line:line[0]).mapValues(lambda line:line[1])
-    dataSet = hashFileLableRDDPair.join(wholeTextFileNameRDD)
-    finalDataSetRDD= dataSet.map(lambda (x,y): (x,y[0],y[1]))
-    
-    # =========================================================================
-    # 1. creating DATAFRAME
-    # 2. Generating NGRAMS
-    # =========================================================================
-    schemaString = "HashCodeFile Label Content"
-    fields = [StructField("HashCodeFile", StringType(), True),StructField("Label", StringType(), True),StructField("Content",ArrayType(StringType(),False), True) ]
-    schema = StructType(fields)
-    schemaByte = spark.createDataFrame(finalDataSetRDD,schema)
-    schemaByte.createOrReplaceTempView("byteDataFrame")
-    ngram = NGram(n=4, inputCol="Content", outputCol="ngrams")
-    ngramDataFrame = ngram.transform(schemaByte)
-    #ngramDataFrame.select("ngrams").show(truncate=False)   \\No Use
-    #ngramDataFrame.show()
+    # Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = rescaledData.randomSplit([0.7, 0.3])
 
-    # =========================================================================
-    # RANDOM FOREST
-    # =========================================================================
-    
-    # labelIndexer = StringIndexer(inputCol="Label", outputCol="indexedLabel").fit(schemaByte)
-    # # Automatically identify categorical features, and index them.
-    # # Set maxCategories so features with > 4 distinct values are treated as continuous.
-    # featureIndexer = VectorIndexer(inputCol="Content", outputCol="indexedFeatures", maxCategories=9).fit(schemaByte)
+    # Train a RandomForest model.
+    rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", numTrees=5)
 
-    # (trainingData, testData) = schemaByte.randomSplit([0.7, 0.3])
+    # Convert indexed labels back to original labels.
+    labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
+                                   labels=labelIndexer.labels)
 
-    # # Train a RandomForest model.
-    # rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures")
+    # Chain indexers and forest in a Pipeline
+    pipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf, labelConverter])
 
-    # # Chain indexers and forest in a Pipeline
-    # pipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf])
+    # Train model.  This also runs the indexers.
+    model = pipeline.fit(trainingData)
 
-    # # Train model.  This also runs the indexers.
-    # model = pipeline.fit(trainingData)
+    # Make predictions.
+    predictions = model.transform(testData)
 
-    # # Make predictions.
-    # predictions = model.transform(testData)
+    # Select example rows to display.
+    predictions.select("predictedLabel", "label", "features").show(5)
 
-    # # Select example rows to display.
-    # predictions.select("prediction", "indexedLabel", "features").show(5)
+    # Select (prediction, true label) and compute test error
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+    accuracy = evaluator.evaluate(predictions)
+    print("Test Error = %g" % (1.0 - accuracy))
 
-    # # Select (prediction, true label) and compute test error
-    # evaluator = MulticlassClassificationEvaluator(
-    #     labelCol="indexedLabel", predictionCol="prediction", metricName="precision")
-    # accuracy = evaluator.evaluate(predictions)
-    # print("Test Error = %g" % (1.0 - accuracy))
-
-    # rfModel = model.stages[2]
-    # print(rfModel)  # summary only
-
+    rfModel = model.stages[2]
+    print(rfModel)
+    #============================================================
 if __name__ == "__main__":
     main()
